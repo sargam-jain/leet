1f3cd346c0dc29225e15d0001bdb1d661ab22e99
Sargamjain813 <141730944+Sargamjain813@users.noreply.github.com>
2024-07-28 21:12:15 +0530

    Time: 20 ms (8.58%), Space: 40.8 MB (43.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,25 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        int y = 4;
-        if(x < 2){
-            return x;
-            
-        }else if(x == 2 || x == 3){
-            return 1;
-        }else if(x == 2147483647){
+        if(x == 2147483647){
             return 46340;
         }
-        else{
         
-      
-        for( y = 2;y < x; y ++){
+      int y = 0;
+        for( y = 0;y <= x; y ++){
             if(y * y == x){
                 return y;
             }else if(y * y > x){
                 return y - 1;
             }
         }
-        }
+        
         
 
         return y;
